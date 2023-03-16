5dfcd460c97f39d6cf35077ab87f8af4275f140a
ander kierig <akierig@umn.edu>
2023-03-16 13:02:19 -0500

    sed's not de[a]d
    need to escape some of the second command or maybe just have sed read
    the input from a file.
    
    I want to break this into functions which would make much more sense at
    this point.

--- geoportal-core-updater.sh
+++ geoportal-core-updater.sh
@@ -8,20 +8,12 @@
 
 set -eu
 
-
+##
 ## permissions problems
 ##	1. the solr core dir is owned by 'ulapps' and not 'uldeploy'
-##		- potential workaround could be to clone the updated solr core somewhere where uldeploy has write permissions and then chown/chmod it to ulapps.
-##		- would maybe need to be a deploy folder in /opt/* which could be chown'd to ulapps and then moved into place?
-##	2. systemctl permisisons for restarting solr
+##		- *solved*: gave uldeploy permission to run git with sudo
 ##
-## ------
-## uldeploy can do the following per /etc/sudoers.d/umnlib-deploy-user
-## chown: 		/bin/chown -R * /opt/data/solr-cores*, /bin/chown -R * /opt/solr-versions*, /bin/chown -R * /opt/var/solr*, /bin/chown -R * /opt/data*, /bin/chmod -R * /opt/data/solr-cores*
-## chmod: 		/bin/chmod -R * /opt/solr-versions*, /bin/chmod -R * /opt/var/solr*, /bin/chmod -R * /opt/data*, 
-## systemctl:	/bin/systemctl * solr, /bin/systemctl * ngin
-## ------
-## we could maybe add the appropriate git commands to let uldeploy run git as ulapps to `geomg-ansible-provision/templates/etc/sudoers.d/umnlib-deploy-user-solr.j2`?
+##	2. systemctl permisisons for restarting solr
 ##
 
 # get the newest solr core git tag
@@ -31,23 +23,25 @@ SOLR_GIT_TAG=$(curl -o- -fsSL "https://api.github.com/repos/geobtaa/geoportal-so
 # set the solr core dir
 SOLR_CORE_DIR=/opt/data/solr-cores/geomg
 
-# idea to solve permission problem 1: create a staging directory (now I have three more problems)
-mkdir -p /opt/solr/core-staging
-chmod 775 /opt/solr/core-staging
-chown -R ulapps:uldeploy /opt/solr/core-staging
-SOLR_STAGING_DIR=/opt/solr/core-staging
-
 # This will need to run as the uldeploy user, so we can restart solr etc right from here.
 sudo /bin/systemctl stop solr.service
 
 # update the solr core itself
+sudo -u ulapps git -C $SOLR_CORE_DIR reset --hard
+sudo -u ulapps git -C $SOLR_CORE_DIR pull --rebase
+sudo -u ulapps git -C $SOLR_CORE_DIR checkout $SOLR_GIT_TAG
 
-git -C $SOLR_CORE_DIR pull --rebase
-git -C $SOLR_CORE_DIR checkout $SOLR_GIT_TAG
+echo -e \n$(sudo -u ulapps git -C $SOLR_CORE_DIR status)\n
 
-echo -e \n$(git -C $SOLR_CORE_DIR status)\n
+# funky dirty terrible, don't try this at home kids
+sudo chown uldeploy:uldeploy $SOLR_CORE_DIR/conf/solrconfig.xml $SOLR_CORE_DIR/core.properties
+mv -f $SOLR_CORE_DIR/conf/solrconfig.xml $SOLR_CORE_DIR/conf/solrconfig.git
+mv -f $SOLR_CORE_DIR/core.properties $SOLR_CORE_DIR/core.properties.git
+sed 's/production/geomg/g' $SOLR_CORE_DIR/core.properties.git > $SOLR_CORE_DIR/core.properties
+# will need to escape some of this
+sed '80g/<requestHandler name="/replication" class="solr.ReplicationHandler" startup="lazy" />'  $SOLR_CORE_DIR/conf/solrconfig.xml.git > $SOLR_CORE_DIR/conf/solrconfig.xml
 
-sudo chown -R ulapps:
+
 
 sudo /bin/systemctl start solr.service
 
